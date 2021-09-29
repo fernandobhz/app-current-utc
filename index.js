6459dc9ed556d4add8bf8de3e8e49f9bd13377fa
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-28 21:48:39 -0300

    fix: port number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const cors = require(`cors`);
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.get(`/`, (req, res) => res.json(new Date().toISOString()));
