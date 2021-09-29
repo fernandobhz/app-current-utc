963dccae4429175cf8beab4927a94a943fefc38c
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-28 21:40:20 -0300

    small app to ping back the current utc time

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require(`express`);
+const cors = require(`cors`);
+
+const app = express();
+const port = process.env.PORT | 3000;
+
+app.use(cors());
+app.get(`/`, (req, res) => res.json(new Date().toISOString()));
+
+app.listen(port, () => console.log(`server is up and running at port: '${port}'`));
